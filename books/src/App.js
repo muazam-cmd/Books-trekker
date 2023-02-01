3ee3ce36bc65d161e827e203eedaf43a64ab8358
muazam-cmd <muazamshahid7@gmail.com>
2023-02-01 14:25:35 +0400

    Implementing states in CreateBooks and adding styles

--- books/src/App.js
+++ books/src/App.js
@@ -1,6 +1,20 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      { id: 123, title }
+    ];
+    setBooks(updatedBooks);
+  }
+
   return <div>
-    App..
+    {books.length}
+    <BookCreate onCreate={createBook} />
   </div>
 }
 
--- books/src/components/BookCreate.js
+++ books/src/components/BookCreate.js
@@ -1,7 +1,28 @@
-function BookCreate() {
-    return <div>
-        Book Create ..
-    </div>
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+
+    const [title, setTitle] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        setTitle('');
+    }
+
+    return (
+        <div className="book-create">
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input className="input" value={title} onChange={handleChange} />
+                <button className="button">Create!</button>
+            </form>
+        </div>
+    );
 }
 
 export default BookCreate;
