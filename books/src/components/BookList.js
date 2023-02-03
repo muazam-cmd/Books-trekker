3104a5e4bf97ce29f538fc368fa0a9e1a43f5908
muazam-cmd <muazamshahid7@gmail.com>
2023-02-03 15:57:03 +0400

    using context system to share data.

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,12 +1,18 @@
+import { useContext } from 'react';
 import BookShow from './BookShow';
+import BooksContext from '../context/books';
 
 function BookList({ books, onDelete, onEdit }) {
+
+    const { count, incrementCount } = useContext(BooksContext);
 
     const renderedBooks = books.map((book) => {
 
         return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
     });
     return <div className='book-list'>
+        {count}
+        <button onClick={incrementCount}>Click</button>
         {renderedBooks}
     </div>
 }
